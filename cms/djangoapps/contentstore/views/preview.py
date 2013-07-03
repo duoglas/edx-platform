e4c765f8f85a981922a5b77163813ffc5f282cef
Anton Stupak <s2pak.anton@gmail.com>
2013-07-03 12:25:38 +0300

    Clean up

--- cms/djangoapps/contentstore/views/preview.py
+++ cms/djangoapps/contentstore/views/preview.py
@@ -71,7 +71,7 @@ def preview_component(request, location):
 
     return render_to_response('component.html', {
         'preview': get_module_previews(request, component)[0],
-        'editor': wrap_xmodule(component.get_html, component, 'xmodule_edit.html')()
+        'editor': wrap_xmodule(component.get_html, component, 'xmodule_edit.html')(),
     })
 
 
