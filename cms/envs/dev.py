3ed0c3fc4b325f316efec894e33d5a5b8ad3388d
Duoglas Du <dujing487@gmail.com>
2013-08-20 11:35:32 +0800

    change lms base url

--- cms/envs/dev.py
+++ cms/envs/dev.py
@@ -64,8 +64,8 @@ DATABASES = {
     }
 }
 
-LMS_BASE = "localhost:8000"
-MITX_FEATURES['PREVIEW_LMS_BASE'] = "localhost:8000"
+LMS_BASE = "10.120.145.221:8000"
+MITX_FEATURES['PREVIEW_LMS_BASE'] = "10.120.145.221:8000"
 
 REPOS = {
     'edx4edx': {
